a6abd726cda5a058a7604df2a78b24059d6dd6a4
Samson Gbose <toyseral@gmail.com>
2026-09-08 23:12:39 +0200

    Add canonical editor completion checklist

---
+++ src/components/admin/CanonicalEditorCompletion.tsx
@@ -0,0 +1,11 @@
+export const CANONICAL_EDITOR_COMPLETION = {
+  globalBestFor: true,
+  countryBestFor: true,
+  brokerWorkspace: true,
+  countryGuides: true,
+  globalGuides: true,
+  brokerAwareBlocks: true,
+  perBrokerComparisonCtas: true,
+  legacyBestForRedirects: true,
+  canonicalSitemapOwnership: true,
+} as const;
