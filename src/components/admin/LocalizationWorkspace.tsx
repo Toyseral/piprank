ae83cef05efa6226a384cc2738d73df09fe981b3
Samson Gbose <toyseral@gmail.com>
2026-09-14 20:40:56 +0200

    refactor: remove legacy localized SEO prop from workspace

--- src/components/admin/LocalizationWorkspace.tsx
+++ src/components/admin/LocalizationWorkspace.tsx
@@ -9,8 +9,6 @@ type Mutate = (path: string, method: string, body: unknown, msg: string) => Prom
 type Props = {
   countries: CountryPage[];
   languages: CountryLanguage[];
-  /** @deprecated Admin no longer reads or writes legacy localized SEO pages. */
-  pages?: unknown[];
   contentDocs: ContentDocument[];
   mutate: Mutate;
   accessToken: string;
