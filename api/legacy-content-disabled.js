bf923a1b3faf72cabd8de2a5317578273d059ef1
Samson Gbose <toyseral@gmail.com>
2026-09-15 11:13:05 +0200

    fix: remove retired API function from Vercel function count

--- api/legacy-content-disabled.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  return res.status(410).json({
-    error: 'This legacy content endpoint has been retired.',
-    code: 'LEGACY_CONTENT_ENDPOINT_RETIRED',
-  });
-}
