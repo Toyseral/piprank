603ece812c9e4a920acfcdc1cb2783e69b4e57ca
Samson Gbose <toyseral@gmail.com>
2026-09-19 01:15:33 +0200

    security: restrict ranking sync functions

---
+++ sql/20260919_restrict_country_intent_ranking_sync_functions.sql
@@ -0,0 +1,3 @@
+REVOKE EXECUTE ON FUNCTION public.sync_country_intent_broker_rankings(bigint) FROM PUBLIC, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION public.sync_intent_country_rankings_on_intent() FROM PUBLIC, anon, authenticated;
+REVOKE EXECUTE ON FUNCTION public.sync_intent_country_rankings_on_broker() FROM PUBLIC, anon, authenticated;
