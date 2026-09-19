31b9256fd5ea350b7b996ba9d43d68f0c64a29fd
Samson Gbose <toyseral@gmail.com>
2026-09-19 10:40:20 +0200

    chore: order security migration after function definitions

--- sql/20260919_harden_new_security_definer_search_paths.sql
+++
@@ -1,6 +0,0 @@
--- Pin search_path for newly introduced SECURITY DEFINER functions.
-alter function public.replace_broker_country_availability(bigint, jsonb)
-  set search_path = '';
-
-alter function public.sync_country_intent_rankings_on_availability()
-  set search_path = '';
